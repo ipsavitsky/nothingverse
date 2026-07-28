0d93facae177f6a36d21a96115dea98b68139b49
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2026-07-28 14:57:45 +0100

    limit to two systems

--- flake.nix
+++ flake.nix
@@ -22,88 +22,90 @@
       rust-overlay,
       treefmt-nix,
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-        naersk-lib = pkgs.callPackage naersk {
-          cargo = pkgs.rust-bin.stable.latest.default;
-          rustc = pkgs.rust-bin.stable.latest.default;
-        };
-        chunkyFont = pkgs.fetchurl {
-          url = "http://www.figlet.org/fonts/chunky.flf";
-          hash = "sha256-A0BwES9Pz9YT8GDxrg5ECHDe/fNjrrMKtG9mnzPXsXM=";
-        };
-        npmHash = import ./nix/npmHash.nix;
-        treefmtModule = treefmt-nix.lib.evalModule pkgs ./nix/treefmt.nix;
-      in
-      rec {
-        formatter = treefmtModule.config.build.wrapper;
+    flake-utils.lib.eachSystem
+      [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+          naersk-lib = pkgs.callPackage naersk {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+          chunkyFont = pkgs.fetchurl {
+            url = "http://www.figlet.org/fonts/chunky.flf";
+            hash = "sha256-A0BwES9Pz9YT8GDxrg5ECHDe/fNjrrMKtG9mnzPXsXM=";
+          };
+          npmHash = import ./nix/npmHash.nix;
+          treefmtModule = treefmt-nix.lib.evalModule pkgs ./nix/treefmt.nix;
+        in
+        rec {
+          formatter = treefmtModule.config.build.wrapper;
 
-        checks = {
-          formatting = treefmtModule.config.build.check self;
-          x86_64-linux = packages.nothingverse;
-        };
+          checks = {
+            formatting = treefmtModule.config.build.check self;
+            x86_64-linux = packages.nothingverse;
+          };
 
-        packages = rec {
-          default = nothingverse;
-          nothingverse = naersk-lib.buildPackage {
-            src = ./.;
-            nativeBuildInputs = with pkgs; [
-              toilet
-              htmlq
-              tailwindcss_4
-            ];
-            overrideMain = old: {
-              nativeBuildInputs = old.nativeBuildInputs ++ [
-                pkgs.nodejs
-                pkgs.npmHooks.npmConfigHook
+          packages = rec {
+            default = nothingverse;
+            nothingverse = naersk-lib.buildPackage {
+              src = ./.;
+              nativeBuildInputs = with pkgs; [
+                toilet
+                htmlq
+                tailwindcss_4
               ];
-              npmDeps = pkgs.fetchNpmDeps {
-                src = ./.;
-                hash = npmHash;
+              overrideMain = old: {
+                nativeBuildInputs = old.nativeBuildInputs ++ [
+                  pkgs.nodejs
+                  pkgs.npmHooks.npmConfigHook
+                ];
+                npmDeps = pkgs.fetchNpmDeps {
+                  src = ./.;
+                  hash = npmHash;
+                };
+                preBuild = ''
+                  # for some reason I have to symlink or toilet can't open the font
+                  tailwindcss -i ./templates/styles-in.css -o ./templates/styles.css
+                  ln -s ${chunkyFont} chunky.flf
+                  toilet -f ./chunky.flf nothingverse --html | htmlq 'body' > templates/logo.html
+                '';
+                SQLX_OFFLINE = true;
               };
-              preBuild = ''
-                # for some reason I have to symlink or toilet can't open the font
-                tailwindcss -i ./templates/styles-in.css -o ./templates/styles.css
-                ln -s ${chunkyFont} chunky.flf
-                toilet -f ./chunky.flf nothingverse --html | htmlq 'body' > templates/logo.html
-              '';
-              SQLX_OFFLINE = true;
+            };
+            modelfile = pkgs.writeTextFile {
+              name = "modelfile";
+              text = builtins.readFile ./ollama/nothing.modelfile;
             };
           };
-          modelfile = pkgs.writeTextFile {
-            name = "modelfile";
-            text = builtins.readFile ./ollama/nothing.modelfile;
-          };
-        };
 
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              pkg-config
-              openssl
-              toilet
-              rust-analyzer
-              watchexec
-              just
-              wget
-              sqlx-cli
-              sqlite
-              tailwindcss_4
-              htmlq
-              zizmor
-              rust-bin.stable.latest.default
-              nodejs
-              nil
-            ];
+          devShells = {
+            default = pkgs.mkShell {
+              packages = with pkgs; [
+                pkg-config
+                openssl
+                toilet
+                rust-analyzer
+                watchexec
+                just
+                wget
+                sqlx-cli
+                sqlite
+                tailwindcss_4
+                htmlq
+                zizmor
+                rust-bin.stable.latest.default
+                nodejs
+                nil
+              ];
+            };
           };
-        };
-      }
-    )
+        }
+      )
     // {
       nixosModules = {
         nothingverse = import ./nix/service.nix { nothingverse = self.packages; };
